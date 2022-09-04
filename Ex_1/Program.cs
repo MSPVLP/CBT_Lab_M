18c8cc372f3cb5857dad971e22a19ab1a9bf6f30
KLA <smtwkla@gmail.com>
2022-09-04 08:49:33 +0530

    while loop added, Console.ReadLine instead of Read (Read() returns Enter and EOF values on second and third iteration of while loop), Added string length check before Char.Parse to prevent exceptions.

--- Ex_1/Program.cs
+++ Ex_1/Program.cs
@@ -8,22 +8,49 @@ namespace Ex_1
         {
             Console.Write("Program to check whether a character is lower or upper case.\n");
             Console.Write("------------------------------------------------\n");
-            Console.Write("Input a character: ");
-            char ch = (char)Console.Read();
+            
+            bool quit = false;
+            string input_str;
+            char ch;
 
-            if (Char.IsUpper(ch))
-            {
-                Console.WriteLine("\nThe character is uppercase.\n");
+            while (!quit) {
+
+                Console.Write("Enter a character [Enter . to quit]: ");
+                input_str = Console.ReadLine();
+
+                if (input_str.Length != 1)               // Check if exactly 1 char entered
+                {
+                    Console.WriteLine("ERROR: Enter exactly 1 charecter only.");
+                    continue;
+                }
+                
+                ch = Char.Parse(input_str);
+
+                if (ch == '.')
+                {
+                    quit = true;
+                    break;
+                }
+
+                if (Char.IsUpper(ch))
+                {
+                    Console.WriteLine("\nThe character is uppercase.\n");
+                }
+                else if (Char.IsLower(ch))
+                {
+                    Console.WriteLine("\nThe character is lowercase.\n");
+                }
+                else
+                {
+                    // User has entered a symbol or number etc. Example: 1,2, #, ?...
+                    Console.WriteLine("\nThe charecter is not an alphabet.\n");
+                }
+
+                // Check if user chose to quit
+
+
             }
-            else if(Char.IsLower(ch))
-            {
-                Console.WriteLine("\nThe character is lowercase.\n");
-            }           
-            else
-            {   
-                // User has entered a symbol or number etc. Example: 1,2, #, ?...
-                Console.WriteLine("\nThe charecter is not an alphabet.\n");
-            }
+            Console.WriteLine("Press any key...");
             Console.ReadKey();
         }
     }
