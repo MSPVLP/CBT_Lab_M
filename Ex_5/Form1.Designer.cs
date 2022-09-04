45da8dc5e21bdb1537baf493bc7f85aa2727fdca
Maniraj <>
2022-09-04 14:19:39 -0700

    Revert "Merge branch 'main' of https://github.com/MSPVLP/CBT_Lab_N"
    This reverts commit 7976e49ca0ea73868b733f2d59f339b0b34e0500, reversing
    changes made to 553471e7ebb4244bc94313a899f1b2c0ea272c4d.

--- Ex_5/Form1.Designer.cs
+++ Ex_5/Form1.Designer.cs
@@ -41,7 +41,6 @@
             this.btn_select_folder = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btn_select_file = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -52,10 +51,9 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(266, 18);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Location = new System.Drawing.Point(177, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(411, 57);
+            this.button1.Size = new System.Drawing.Size(274, 37);
             this.button1.TabIndex = 8;
             this.button1.Text = "Folder and File Control ";
             this.button1.UseVisualStyleBackColor = true;
@@ -64,124 +62,102 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(32, 486);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(32, 353);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(162, 29);
+            this.label2.Size = new System.Drawing.Size(101, 20);
             this.label2.TabIndex = 12;
-            this.label2.Text = "Selected File:";
+            this.label2.Text = "Selected File";
             // 
             // txt_filename
             // 
             this.txt_filename.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_filename.Location = new System.Drawing.Point(37, 520);
-            this.txt_filename.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txt_filename.Location = new System.Drawing.Point(75, 390);
             this.txt_filename.Name = "txt_filename";
-            this.txt_filename.Size = new System.Drawing.Size(674, 35);
+            this.txt_filename.Size = new System.Drawing.Size(439, 26);
             this.txt_filename.TabIndex = 11;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(32, 386);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(22, 257);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(192, 29);
+            this.label1.Size = new System.Drawing.Size(121, 20);
             this.label1.TabIndex = 10;
-            this.label1.Text = "Selected Folder:";
+            this.label1.Text = "Selected Folder";
             // 
             // txt_foldername
             // 
             this.txt_foldername.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_foldername.Location = new System.Drawing.Point(37, 420);
-            this.txt_foldername.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txt_foldername.Location = new System.Drawing.Point(75, 294);
             this.txt_foldername.Name = "txt_foldername";
-            this.txt_foldername.Size = new System.Drawing.Size(674, 35);
+            this.txt_foldername.Size = new System.Drawing.Size(439, 26);
             this.txt_foldername.TabIndex = 9;
             // 
             // dirListBox1
             // 
             this.dirListBox1.FormattingEnabled = true;
             this.dirListBox1.IntegralHeight = false;
-            this.dirListBox1.Location = new System.Drawing.Point(37, 136);
-            this.dirListBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dirListBox1.Location = new System.Drawing.Point(162, 69);
             this.dirListBox1.Name = "dirListBox1";
-            this.dirListBox1.Size = new System.Drawing.Size(356, 225);
+            this.dirListBox1.Size = new System.Drawing.Size(239, 172);
             this.dirListBox1.TabIndex = 13;
             this.dirListBox1.SelectedIndexChanged += new System.EventHandler(this.dirListBox1_SelectedIndexChanged);
             // 
             // driveListBox1
             // 
             this.driveListBox1.FormattingEnabled = true;
-            this.driveListBox1.Location = new System.Drawing.Point(37, 99);
-            this.driveListBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.driveListBox1.Location = new System.Drawing.Point(22, 69);
             this.driveListBox1.Name = "driveListBox1";
-            this.driveListBox1.Size = new System.Drawing.Size(356, 27);
+            this.driveListBox1.Size = new System.Drawing.Size(121, 21);
             this.driveListBox1.TabIndex = 14;
             this.driveListBox1.SelectedIndexChanged += new System.EventHandler(this.driveListBox1_SelectedIndexChanged);
             // 
             // fileListBox1
             // 
             this.fileListBox1.FormattingEnabled = true;
-            this.fileListBox1.Location = new System.Drawing.Point(413, 99);
-            this.fileListBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.fileListBox1.Location = new System.Drawing.Point(419, 69);
             this.fileListBox1.Name = "fileListBox1";
             this.fileListBox1.Pattern = "*.*";
-            this.fileListBox1.Size = new System.Drawing.Size(298, 264);
+            this.fileListBox1.Size = new System.Drawing.Size(200, 173);
             this.fileListBox1.TabIndex = 15;
             this.fileListBox1.SelectedIndexChanged += new System.EventHandler(this.fileListBox1_SelectedIndexChanged);
             // 
             // btn_select_folder
             // 
             this.btn_select_folder.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_select_folder.Location = new System.Drawing.Point(735, 421);
-            this.btn_select_folder.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_select_folder.Location = new System.Drawing.Point(532, 294);
             this.btn_select_folder.Name = "btn_select_folder";
-            this.btn_select_folder.Size = new System.Drawing.Size(176, 35);
+            this.btn_select_folder.Size = new System.Drawing.Size(102, 23);
             this.btn_select_folder.TabIndex = 16;
-            this.btn_select_folder.Text = "Select Folder...";
+            this.btn_select_folder.Text = "Select Folder";
             this.btn_select_folder.UseVisualStyleBackColor = true;
             this.btn_select_folder.Click += new System.EventHandler(this.btn_select_folder_Click);
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(37, 646);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Location = new System.Drawing.Point(655, 69);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(851, 366);
+            this.pictureBox1.Size = new System.Drawing.Size(205, 173);
             this.pictureBox1.TabIndex = 17;
             this.pictureBox1.TabStop = false;
             // 
             // btn_select_file
             // 
             this.btn_select_file.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_select_file.Location = new System.Drawing.Point(735, 520);
-            this.btn_select_file.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_select_file.Location = new System.Drawing.Point(532, 390);
             this.btn_select_file.Name = "btn_select_file";
-            this.btn_select_file.Size = new System.Drawing.Size(176, 35);
+            this.btn_select_file.Size = new System.Drawing.Size(102, 23);
             this.btn_select_file.TabIndex = 18;
-            this.btn_select_file.Text = "Select File...";
+            this.btn_select_file.Text = "Select File";
             this.btn_select_file.UseVisualStyleBackColor = true;
             this.btn_select_file.Click += new System.EventHandler(this.btn_select_file_Click);
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(32, 612);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(94, 29);
-            this.label3.TabIndex = 19;
-            this.label3.Text = "Picture:";
-            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(946, 1091);
-            this.Controls.Add(this.label3);
+            this.ClientSize = new System.Drawing.Size(872, 494);
             this.Controls.Add(this.btn_select_file);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btn_select_folder);
@@ -193,7 +169,6 @@
             this.Controls.Add(this.txt_filename);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txt_foldername);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Folder and File Control  - Ex 5";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -217,7 +192,6 @@
         private System.Windows.Forms.Button btn_select_folder;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button btn_select_file;
-        private System.Windows.Forms.Label label3;
     }
 }
 
--- Ex_5/Form1.cs
+++ Ex_5/Form1.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.VisualBasic.Compatibility.VB6;
-using System;
+﻿using System;
 using System.Windows.Forms;
 
 namespace Ex_5
@@ -10,45 +9,31 @@ namespace Ex_5
         {
             InitializeComponent();
         }      
-
-        private void driveListBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            // Update Directory List box with changed drive letter
-            dirListBox1.Path = driveListBox1.Drive;
-        }
-
-        private void dirListBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            // Update File List Box with changed folder selection
-            fileListBox1.Path = dirListBox1.get_DirList(dirListBox1.DirListIndex);
-        }
-
         private void fileListBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            // Update text boxes with folder and file name
             txt_foldername.Text = fileListBox1.Path;
-            txt_filename.Text = fileListBox1.FileName;
-            
-            // Load the file in picture box
-            pictureBox1.ImageLocation = fileListBox1.Path + "\\" + fileListBox1.FileName;
+            txt_foldername.Text = dirListBox1.Path;
+            txt_filename.Text = (fileListBox1.FileName);
+            // Loads the image in picture box
+            pictureBox1.ImageLocation = dirListBox1.Path + "\\" + fileListBox1.FileName;
         }
-
+        private void driveListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dirListBox1.Path = driveListBox1.Drive;
+        }
+        private void dirListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            fileListBox1.Path = dirListBox1.Path;
+        }
         private void btn_select_folder_Click(object sender, EventArgs e)
         {
-            DialogResult result = folderBrowserDialog1.ShowDialog();
-            if (result == DialogResult.OK)                  // Check if user clicked cancel or OK
-                txt_foldername.Text = fileListBox1.Path;    // Update Folder Name Textbox 
+            folderBrowserDialog1.ShowDialog();
+            txt_foldername.Text = folderBrowserDialog1.SelectedPath;
         }
-
         private void btn_select_file_Click(object sender, EventArgs e)
         {
-            DialogResult result = openFileDialog1.ShowDialog();
-            if (result == DialogResult.OK)
-            {
-                txt_filename.Text = openFileDialog1.FileName;
-                pictureBox1.ImageLocation = openFileDialog1.FileName;
-            }
+            openFileDialog1.ShowDialog();
+            txt_filename.Text = openFileDialog1.FileName;
         }
-
     }
 }
