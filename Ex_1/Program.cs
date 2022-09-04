b8c855d5b60fea4fd2861904548332d61e91d68b
KLA <smtwkla@gmail.com>
2022-09-04 08:23:50 +0530

    fix: if symbol/number is entered, program prints lowercase. now fixed with additional condition.

--- Ex_1/Program.cs
+++ Ex_1/Program.cs
@@ -6,18 +6,24 @@ namespace Ex_1
     {
         static void Main(string[] args)
         {
-            Console.Write("Check whether a character is lower or upper case\n");
+            Console.Write("Program to check whether a character is lower or upper case.\n");
             Console.Write("------------------------------------------------\n");
             Console.Write("Input a character: ");
             char ch = (char)Console.Read();
-              if (Char.IsUpper(ch))
-                {
-                    Console.WriteLine("\nThe character is uppercase.\n");
-                }
-                else
-                {
-                    Console.WriteLine("\nThe character is lowercase.\n");
-                }           
+
+            if (Char.IsUpper(ch))
+            {
+                Console.WriteLine("\nThe character is uppercase.\n");
+            }
+            else if(Char.IsLower(ch))
+            {
+                Console.WriteLine("\nThe character is lowercase.\n");
+            }           
+            else
+            {   
+                // User has entered a symbol or number etc. Example: 1,2, #, ?...
+                Console.WriteLine("\nThe charecter is not an alphabet.\n");
+            }
             Console.ReadKey();
         }
     }
