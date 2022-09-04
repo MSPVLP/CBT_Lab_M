5decc0cc51d20018171cdc0f6d1097079c6d899c
KLA <smtwkla@gmail.com>
2022-09-05 00:59:23 +0530

    ConnectionString containing literal \ can use @ verbatim string literal to suppress escape seq etc., Instead of hardcoded path, use Application.StartupPath - points to Debug folder of exe.

--- Ex_6/Form1.cs
+++ Ex_6/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Data;
 using System.Data.SqlClient;
+using System.IO;
 using System.Windows.Forms;
 
 namespace Ex_6
@@ -11,24 +12,29 @@ namespace Ex_6
         {
             InitializeComponent();
         }
+
         SqlConnection con = new SqlConnection();
         SqlCommand cmd = new SqlCommand();
+
         private void btn_save_Click(object sender, EventArgs e)
         {
-            con.ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=c:\\users\\maniraj\\documents\\visual studio 2015\\Projects\\PartA\\student_db.mdf;Integrated Security=True";
+            con.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + Application.StartupPath + @"\student_db.mdf;Integrated Security=True";
+            System.Diagnostics.Debug.WriteLine(con.ConnectionString);
             con.Open();
 
-            cmd = new SqlCommand("insert into student_master(rollno,student_name,dept_name,address,mobileno)values(@rno,@stuname,@dept,@address,@mobileno)", con);
+            cmd = new SqlCommand("insert into student_master (rollno,student_name,dept_name,address,mobileno) values (@rno,@stuname,@dept,@address,@mobileno)", con);
             cmd.Parameters.Add("@rno", SqlDbType.Int).Value = txt_rno.Text;
             cmd.Parameters.Add("@stuname", SqlDbType.VarChar).Value = txt_stuname.Text;
             cmd.Parameters.Add("@dept", SqlDbType.VarChar).Value = cmb_deptname.SelectedItem.ToString();
             cmd.Parameters.Add("@address", SqlDbType.VarChar).Value = txt_address.Text;
             cmd.Parameters.Add("@mobileno", SqlDbType.VarChar).Value = txt_mobile.Text;
-            int x = cmd.ExecuteNonQuery();
-            if (x == 1)
-                MessageBox.Show("Students Data Inserted");
+
+            int result = cmd.ExecuteNonQuery();
+
+            if (result == 1)
+                MessageBox.Show("Student's Data Inserted.");
             else
-                MessageBox.Show("Students Data Not Inserted");
+                MessageBox.Show("Student's Data Not Inserted.");
 
             con.Close();
         }
