7215d465c00f056b24f2dc432730c32654c43980
Maniraj <>
2022-09-04 13:53:54 -0700

    Updated Part B

---
+++ Ex_14/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Ex_14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_save = new System.Windows.Forms.Button();
+            this.rdb_select = new System.Windows.Forms.RadioButton();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_save
+            // 
+            this.btn_save.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_save.Location = new System.Drawing.Point(364, 391);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(187, 32);
+            this.btn_save.TabIndex = 29;
+            this.btn_save.Text = "Write Data to XML";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // rdb_select
+            // 
+            this.rdb_select.AutoSize = true;
+            this.rdb_select.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdb_select.Location = new System.Drawing.Point(126, 39);
+            this.rdb_select.Name = "rdb_select";
+            this.rdb_select.Size = new System.Drawing.Size(342, 29);
+            this.rdb_select.TabIndex = 28;
+            this.rdb_select.Text = "Load Students Data From Database";
+            this.rdb_select.UseVisualStyleBackColor = true;
+            this.rdb_select.CheckedChanged += new System.EventHandler(this.rdb_select_CheckedChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(30, 93);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(521, 292);
+            this.dataGridView1.TabIndex = 27;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 471);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.rdb_select);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.RadioButton rdb_select;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Ex_14/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Ex_14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //CONNECTION OBJECT
+        SqlConnection con = new SqlConnection();
+        // COMMAND OBJECT
+        SqlCommand cmd = new SqlCommand(); 
+        DataSet ds = new DataSet();
+        private void rdb_select_CheckedChanged(object sender, EventArgs e)
+        {
+            con.ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=E:\\CBT_LabCode\\CBT_Lab_N\\Ex_14\\student_details_db_14.mdf;Integrated Security=True;Integrated Security=True";
+            con.Open();
+           
+            cmd.Connection = con;
+            cmd.CommandText = "select * from student_master ";
+            SqlDataAdapter ada = new SqlDataAdapter();
+            ada.SelectCommand = cmd;
+            ada.Fill(ds, "student_master");
+            dataGridView1.DataSource = ds;
+            dataGridView1.DataMember = "student_master";
+
+        }
+
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            ds.WriteXml("E:\\CBT_LabCode\\CBT_Lab_N\\Ex_14\\student_details.xml", XmlWriteMode.WriteSchema);
+            con.Close();
+            MessageBox.Show("Sucessfully Written students data from database to XML...");
+
+        }
+    }
+}
