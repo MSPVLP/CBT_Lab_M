75f7e123b5b8ff875387ea5f9a2eb74c546c8a59
KLA <smtwkla@gmail.com>
2022-09-15 06:18:50 +0530

    load_question & btn_start_Click cleanup, timer display updated in else block, cosmetic: comments added, changed

--- Ex_9/Form1.cs
+++ Ex_9/Form1.cs
@@ -22,7 +22,7 @@ namespace Ex_9
 
         string[] questions = new string[]
         {
-                "Which of these is a fruits?",
+                "Which of these is a fruit?",
                 "What OS did Linus develop?",
                 "What is India's capital?",
                 "________ is a loop in C#.",
@@ -49,12 +49,14 @@ namespace Ex_9
 
         public void reset_quiz()
         {
+            // Reset score, question_no variables
             current_question = -1;
             score = 0;
             txt_score.Text = score.ToString();
             txt_question_no.Text = "";
             txt_question.Text = "";
 
+            // Disable and clear all quiz question controls
             rdb_A.Text = "";
             rdb_B.Text = "";
             rdb_C.Text = "";
@@ -70,40 +72,47 @@ namespace Ex_9
             rdb_C.Enabled = false;
             rdb_D.Enabled = false;
 
+            // Show start button and hide next btn
             btn_start.Enabled = true;
             btn_next_question.Visible = false;
         }
 
         private void btn_start_Click(object sender, EventArgs e)
         {
+            // Start Quiz
+            current_question = 0;            // Start from Question index 0
+            load_question(current_question); // Load first question
+
+            btn_start.Enabled = false;       // Start button not needed
+            btn_next_question.Visible = true; // Next question btn needed
+            
             start_time = DateTime.Now;       // Record start time of quiz
-            current_question = 0;              // Start from Question index 0
-            load_question(current_question);   // Load first question
-            timer1.Start();
+            timer1.Start();                  // Start timer
         }
 
         public void load_question(int q_no)
         {
-            txt_question_no.Text = (q_no + 1).ToString();
+            // Load question q_no into quiz controls
+            txt_question_no.Text = (q_no + 1).ToString();   //  q_no = 0 is question no. 1
             txt_question.Text = questions[q_no];
 
+            // Load answer choices for the question into radio buttons
             rdb_A.Text = answer_choices[q_no, 0];
             rdb_B.Text = answer_choices[q_no, 1];
             rdb_C.Text = answer_choices[q_no, 2];
             rdb_D.Text = answer_choices[q_no, 3];
 
+            // Clear any previous selections by user 
             rdb_A.Checked = false;
             rdb_B.Checked = false;
             rdb_C.Checked = false;
             rdb_D.Checked = false;
 
+            // Enable all buttons
             rdb_A.Enabled = true;
             rdb_B.Enabled = true;
             rdb_C.Enabled = true;
             rdb_D.Enabled = true;
-
-            btn_start.Enabled = false;
-            btn_next_question.Visible = true;
         }
 
         public void check_answer()
@@ -127,11 +136,12 @@ namespace Ex_9
                 score -= neg_marks_for_wrong;
             }
             
-            txt_score.Text = Convert.ToString(score);       // Update score 
+            txt_score.Text = Convert.ToString(score);       // Update score display
         }
 
         private void btn_next_question_Click(object sender, EventArgs e)
         {
+            // Check answer to current question
             check_answer();
 
             // Go to next question
@@ -145,16 +155,14 @@ namespace Ex_9
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            TimeSpan span = DateTime.Now.Subtract(start_time);
-            time_remaining = MAX_QUIZ_SECONDS - span.Seconds;
+            // Time elapsed = Now - start_time
+            TimeSpan quiz_time_taken = DateTime.Now.Subtract(start_time);
+            time_remaining = MAX_QUIZ_SECONDS - quiz_time_taken.Seconds;
 
-            txt_time.Text = time_remaining.ToString();
-
-            if (span.Seconds == MAX_QUIZ_SECONDS)
-            {
-                timer1.Stop();
+            if (quiz_time_taken.Seconds == MAX_QUIZ_SECONDS)
                 quiz_end("You ran out of time.");
-            }
+            else 
+                txt_time.Text = time_remaining.ToString();  // Update timer display
         }
 
         public void quiz_end(string msg)
